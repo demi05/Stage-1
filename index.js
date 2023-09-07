f05baa72860e3ba725016e2eb21cec1e7657c489
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2023-09-07 18:40:15 +0100

    committing first changes

---
+++ index.js
@@ -0,0 +1,20 @@
+// date script
+let currentDay0fTheWeek= document.querySelector("#currentDay0fTheWeek")
+
+const date= new Date()
+console.log(date)
+let currentDay= date.getDate()
+let currentMonth= date.getMonth()+1
+let currentYear= date.getFullYear()
+
+currentDay0fTheWeek.innerHTML=`
+<p>Today's date is:  ${currentDay}/${currentMonth}/${currentYear}</p>
+`
+
+// time script
+let currentUTCTime= document.querySelector("#currentUTCTime")
+let time=date.toUTCString().slice(17,22)
+console.log(time)
+currentUTCTime.innerHTML=`
+<p> The time is  ${time}</p>
+`
